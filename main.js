c7fb8240accc912fa5334ee7883815d81fe66c5e
mad <pagutaad@students.nu-lipa.edu.ph>
2025-05-05 05:50:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var type = new Typed(".typewriter-text", {
+    strings: ["Frontend Developer", "Web Designer", "UI/UX Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
